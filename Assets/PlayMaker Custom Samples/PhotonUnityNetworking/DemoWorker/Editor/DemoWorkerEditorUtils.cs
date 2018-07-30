a2822ceb966f249862ba7d83afdcff74fa1936fa
Jean Fabre <contact@fabrejean.net>
2018-07-30 10:39:26 +0300

    Updated Demo to have a scripted way to define global event while PlayMaker future update deals with it

---
+++ Assets/PlayMaker Custom Samples/PhotonUnityNetworking/DemoWorker/Editor/DemoWorkerEditorUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+using HutongGames.PlayMakerEditor;
+
+[InitializeOnLoad]
+public class DemoWorkerEditorUtils
+{
+
+	static bool _eventAdded;
+
+
+	static string[] _eventList = new string[]{
+
+		"ROOM IS BUSY",
+		"ROOM IS EMPTY",
+		"CHAT RPC CALL",
+		"DOOR RPC CALL"
+
+	};
+
+	static DemoWorkerEditorUtils()
+	{
+		#if PLAYMAKER_1_9_OR_NEWER
+		FsmEditorSettings.ShowNetworkSync = true;
+		#endif
+
+
+		// add global events if needed.
+		if (!_eventAdded)
+		{
+			foreach (string _event in _eventList)
+			{
+				_eventAdded = PlayMakerUtils.CreateIfNeededGlobalEvent (_event);
+			}
+		}
+
+	}
+
+}
