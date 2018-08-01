dec0c4e99633b767e0625c5843486d44dcdda162
Jean Fabre <contact@fabrejean.net>
2018-08-01 11:40:37 +0300

    improved checks for DemoWorker missing global events

--- Assets/PlayMaker Custom Samples/PhotonUnityNetworking/DemoWorker/Editor/DemoWorkerEditorUtils.cs
+++ Assets/PlayMaker Custom Samples/PhotonUnityNetworking/DemoWorker/Editor/DemoWorkerEditorUtils.cs
@@ -10,33 +10,12 @@ using HutongGames.PlayMakerEditor;
 public class DemoWorkerEditorUtils
 {
 
-	static bool _eventAdded;
-
-
-	static string[] _eventList = new string[]{
-
-		"ROOM IS BUSY",
-		"ROOM IS EMPTY",
-		"CHAT RPC CALL",
-		"DOOR RPC CALL"
-
-	};
-
 	static DemoWorkerEditorUtils()
 	{
 		#if PLAYMAKER_1_9_OR_NEWER
 		FsmEditorSettings.ShowNetworkSync = true;
 		#endif
 
-
-		// add global events if needed.
-		if (!_eventAdded)
-		{
-			foreach (string _event in _eventList)
-			{
-				_eventAdded = PlayMakerUtils.CreateIfNeededGlobalEvent (_event);
-			}
-		}
 
 	}
 
---
+++ Assets/PlayMaker Custom Samples/PhotonUnityNetworking/DemoWorker/Script/DemoWorkerSetup.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HutongGames.PlayMaker;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// This is  to force global events
+/// </summary>
+[ExecuteInEditMode]
+public class DemoWorkerSetup : MonoBehaviour {
+
+
+    static bool _eventCreated;
+
+    static string[] _eventList = new string[]{
+
+        "ROOM IS BUSY",
+        "ROOM IS EMPTY",
+        "CHAT RPC CALL",
+        "DOOR RPC CALL"
+
+    };
+
+
+    public GameObject WarningCanvas;
+
+
+    void Update()
+    {
+
+#if UNITY_EDITOR
+        if (!EditorApplication.isPlaying)
+        {
+            bool _needsSetup = false;
+
+            foreach(string _event in _eventList)
+            {
+                _needsSetup = _needsSetup || !FsmEvent.IsEventGlobal(_event);
+
+            }
+
+            if (WarningCanvas != null && WarningCanvas.activeInHierarchy != _needsSetup)
+            {
+                WarningCanvas.SetActive(_needsSetup);
+            }
+
+        }
+#endif
+
+    }
+
+
+}
